3f884c6752d445bfdb75916c132739173ac66047
syedmohammad-ali <syed.ascendifyr@gmail.com>
2024-03-09 13:53:05 +0530

    Adding Signup

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-HelLO wORLD 
+    <header>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
     </>
   )
 }
